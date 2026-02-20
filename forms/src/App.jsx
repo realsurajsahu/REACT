f741e12da6a8deda0ce6c060c4684660e8eb12c7
Suraj Sahu <surajsrsahu@gmail.com>
2026-02-20 21:02:10 +0530

    two way binding added result on console

--- forms/src/App.jsx
+++ forms/src/App.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 const App = () => {
+
+  const [Text, setText] = useState('')
+
   const submit = (e) => {
-e.preventDefault()
-console.log('Submitted');
-    }
+    e.preventDefault();
+    console.log("Submitted by", Text );
+  };
 
   return (
     <div>
-      <form onSubmit={(e)=>{submit(e)}}>
-        <input type="text" placeholder='name'/>
+      <form
+        onSubmit={(e) => {
+          submit(e);
+        }}
+      >
+        <input
+          type="text"
+          placeholder="name"
+          value={Text}
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+        />
         <button>Submit</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
