4d3d9d5a13a4194008cbd2cba72f43fe07c78c79
Suraj Sahu <surajsrsahu@gmail.com>
2026-05-19 18:44:07 +0530

    new project notes app

--- gallery/src/App.jsx
+++ gallery/src/App.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      hi
+    <div className='p-6'>
+      <h1 className='text-center text-4xl pb-4'>Gallery App</h1>
+      <hr />
+      
     </div>
   )
 }
---
+++ notes-updated/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-full h-full'></div>
+  )
+}
+
+export default App
